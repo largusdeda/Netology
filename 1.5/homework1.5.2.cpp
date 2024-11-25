9f123bc4d243c84094cc20f7dab9e5652cee527a
largusdeda <130408211+largusdeda@users.noreply.github.com>
2024-11-25 23:22:03 +0300

    Update homework1.5.2.cpp

--- 1.5/homework1.5.2.cpp
+++ 1.5/homework1.5.2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
 int main() {
-  int arr1[] = { -4, 1, 0, 666, -2, 5, -1, 4, -864788, 2 };
-  int min1 = arr1[0], max1 = min1;
+  int arr[] = { -4, 1, 1, 666, -2, 556, -1453, 4, -3738, 2 };
+  int min = arr[0], max = min;
 
   std::cout << "Массив: ";
-  for (auto num : arr1) {
+  for (auto num : arr) {
     std::cout << num << " ";
-    if (num < min1) {
-      min1 = num;
+    if (num < min) {
+      min = num;
     }
-    if (num > max1) {
-      max1 = num;
+    if (num > max) {
+      max = num;
     }
   }
-  std::cout << "\nМинимальный элемент массива: " << min1  << "\nМаксимальный элемент массива: " << max1 << std::endl;
+  std::cout << "\nМинимальный элемент массива: " << min  << "\nМаксимальный элемент массива: " << max << std::endl;
 
   return EXIT_SUCCESS;
 }
