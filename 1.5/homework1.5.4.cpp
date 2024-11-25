2de5ddb5512bba53b54e0015ec539bf2c3d34cfb
largusdeda <130408211+largusdeda@users.noreply.github.com>
2024-11-26 00:41:03 +0300

    Add files via upload

---
+++ 1.5/homework1.5.4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+	int arr[] = { 10, 5738, 2232, -47382, -78, 666, 10, 33, -666, -1 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	bool swapped = false;
+
+	std::cout << "Массив до сортировки: ";
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	do {
+		swapped = false;
+		for (int i = size - 1; i > 0; i--) {
+			if (arr[i - 1] > arr[i]) {
+				int tmp = arr[i];
+				arr[i] = arr[i - 1];
+				arr[i - 1] = tmp;
+				swapped = true;
+			}
+		}
+	} while (swapped);
+
+	std::cout << "Массив после сортировки: ";
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	return EXIT_SUCCESS;
+}
