d5fdd960dbb0c5919cfd049d23e25064565c3f9d
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2024-11-28 22:20:55 +0300

    Create homework1.6.3.cpp

---
+++ 1.6/homework1.6.3.cpp
@@ -0,0 +1,29 @@
+//Числа Фибоначчи без рекурсии
+
+#include <iostream>
+
+unsigned long long fibonacci(int n) {
+	unsigned long long a{ 0 };
+	unsigned long long b{ 1 };
+
+	while (n != 0) {
+		a = a + b;
+		b = a - b;
+		--n;
+	}
+
+	return a;
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n{};
+	std::cout << "Введите число больше нуля: ";
+	std::cin >> n;
+	std::cout << "Числа Фибоначчи: ";
+	for (int i{ 0 }; i < n; i++) {
+		std::cout << fibonacci(i) << " ";
+	}
+
+	return EXIT_SUCCESS;
+}
