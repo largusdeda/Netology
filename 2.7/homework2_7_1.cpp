d7a896fe1f04662ce1063d23f0ddf101db1e9592
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-02-24 21:26:32 +0300

    Add files via upload

---
+++ 2.7/homework2_7_1.cpp
@@ -0,0 +1,31 @@
+﻿#define MODE 1
+
+#ifndef MODE
+#error Необходимо определить константу MODE
+#endif // !MODE
+
+#if MODE == 1
+int add(int a, int b) {
+	return a + b;
+}
+#endif
+
+#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "rus");
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	int a{}, b{};
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+	std::cout << "Введите число 2: ";
+	std::cin >> b;
+	std::cout << "Результат сложения: " << add(a, b);
+#else 
+	std::cout << "Неизвестный режим. Завершение работы" << std:: endl;
+#endif
+	return EXIT_SUCCESS;
+}
---
+++ 2.7/homework2_7_2.cpp
@@ -0,0 +1,14 @@
+﻿#define SUB(a, b) ((a) - (b))
+
+#include <iostream>
+
+int main() {
+	int a{6};
+	int b{5};
+	int c{2};
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+	return EXIT_SUCCESS;
+}
