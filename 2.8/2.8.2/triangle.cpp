1baa454302f857e935aa8e51f0c8adfdd9ac8d49
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-02-27 00:22:18 +0300

    Update triangle.cpp

--- 2.8/2.8.2/triangle.cpp
+++ 2.8/2.8.2/triangle.cpp
@@ -1,17 +1,16 @@
 #include "triangle.h"
 #include "invalid_figure_error.h"
-#include <iostream>
 
 Triangle::Triangle(int a, int b, int c, int A, int B, int C) {
 	if (A + B + C != 180)
-		throw invalid_figure_error("ошибка создания фигуры. Причина: сумма углов не равна 180\n");
+		throw invalid_figure_error("РѕС€РёР±РєР° СЃРѕР·РґР°РЅРёСЏ С„РёРіСѓСЂС‹. РџСЂРёС‡РёРЅР°: СЃСѓРјРјР° СѓРіР»РѕРІ РЅРµ СЂР°РІРЅР° 180\n");
 	this->a = a;
 	this->b = b;
 	this->c = c;
 	this->A = A;
 	this->B = B;
 	this->C = C;
-	name = "Треугольник";
+	name = "РўСЂРµСѓРіРѕР»СЊРЅРёРє";
 }
 
 std::string Triangle::get_name() {
@@ -19,9 +18,13 @@ std::string Triangle::get_name() {
 }
 
 std::string Triangle::get_sides() {
-	return "стороны: a = " + std::to_string(a) + ", b = " + std::to_string(b) + ", c = " + std::to_string(c);
+	return "СЃС‚РѕСЂРѕРЅС‹: a = " + std::to_string(a) + ", b = " + std::to_string(b) + ", c = " + std::to_string(c);
 }
 
 std::string Triangle::get_angles() {
-	return "углы: A = " + std::to_string(A) + ", B = " + std::to_string(B) + ", C = " + std::to_string(C);
+	return "СѓРіР»С‹: A = " + std::to_string(A) + ", B = " + std::to_string(B) + ", C = " + std::to_string(C);
+}
+
+std::string Triangle::get_angles() {
+	return "ГіГЈГ«Г»: A = " + std::to_string(A) + ", B = " + std::to_string(B) + ", C = " + std::to_string(C);
 }
