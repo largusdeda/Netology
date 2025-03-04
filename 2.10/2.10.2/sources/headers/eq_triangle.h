2e3ccdb4804705530ec24c26729a11889f3cfbd9
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-03-04 22:29:30 +0300

    Delete 2.10/2.10.2/sources directory

--- 2.10/2.10.2/sources/headers/eq_triangle.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "isos_triangle.h"
-
-class Equilateral_triangle : public Isosceles_triangle {
-public:
-	Equilateral_triangle(int a);
-};
--- 2.10/2.10.2/sources/headers/isos_triangle.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "triangle.h"
-
-class Isosceles_triangle : public Triangle {
-public:
-	Isosceles_triangle(int a, int b, int A, int B);
-};
