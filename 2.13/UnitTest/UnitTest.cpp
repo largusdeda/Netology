bbc543abc3fc00331eb000370eeedb6a809b7fa5
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-04-01 23:10:18 +0300

    Add files via upload

---
+++ 2.13/UnitTest/UnitTest.cpp
@@ -0,0 +1,62 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../extended_array.h"
+#include <exception>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest {
+	TEST_CLASS(ExtendedArrayTest) {
+	public:
+		
+		TEST_METHOD(Mean) {
+			ExtArray<int> v1{ 4, 2, 7, 3, -5, 0, -3, 1 };
+			Assert::AreEqual(v1.mean(), 1.125);
+		}
+		TEST_METHOD(Median) {
+			ExtArray<double> v2{ 4.5, 0.7, 10.8, -3.5 };
+			Assert::AreEqual(v2.median(), 2.6);
+		}
+		TEST_METHOD(Mode) {
+			ExtArray<int> v1{ 4, 2, 7, 2, 5, -5, 0, 5, 5 };
+			Assert::AreEqual(v1.mode().first, 5);
+		}
+
+		TEST_METHOD(Mean_EmptyArray) {
+			ExtArray<int> v1(0);
+			Assert::AreEqual(v1.mean(), 0.0);
+		}
+		TEST_METHOD(Median_EmptyArray) {
+			ExtArray<double> v2(0);
+			Assert::AreEqual(v2.median(), 0.0);
+		}
+		TEST_METHOD(Mode_Empty_Array) {
+			ExtArray<int> v1(0);
+			Assert::AreEqual(v1.mode().first, 0);
+		}
+
+		TEST_METHOD(Mean_InRange_Valid) {
+			ExtArray<int> v1{ 4, 2, 7, 3, -5, 0, -3, 1 };
+			Assert::AreEqual(v1.mean(0, 3), 4.0);
+		}
+		TEST_METHOD(Mean_InRange_Invalid) {
+			ExtArray<int> v1{ 4, 2, 7, 3, -5, 0, -3, 1 };
+			auto method = [&] { v1.mean(-1, 7); };
+			Assert::ExpectException<std::invalid_argument>(method);
+		}
+
+		TEST_METHOD(CheckSum_Bool) {
+			ExtArray<bool> v3{ true, false, false, true };
+			Assert::AreEqual(v3.checkSum(), 2);
+		}
+		TEST_METHOD(CheckSum_Int) {
+			ExtArray<int> v3{ 1, 0, 0, 1 };
+			Assert::AreEqual(v3.checkSum(), 2);
+		}
+		TEST_METHOD(CheckSum_BadType) {
+			ExtArray<double> v3{ 1.0, 0.0, 0.0, 1.0 };
+			auto method = [&] { v3.checkSum(); };
+			Assert::ExpectException<std::bad_typeid>(method);
+		}
+	};
+}
---
+++ 2.13/extended_array.h
@@ -0,0 +1,109 @@
+﻿#pragma once
+#include <utility>
+#include <initializer_list>
+#include <deque>
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+class ExtArray
+{
+private:
+	//std::vector<T> extended_array;
+	std::deque<T> extended_array;
+	size_t _size;
+public:
+	ExtArray(std::initializer_list<T> l) : extended_array(l)
+	{
+		_size = l.size();
+	}
+
+	ExtArray(int size) : _size(size)
+	{
+		extended_array.resize(_size);
+	}
+
+	T& operator[](size_t index)
+	{
+		return extended_array[index];
+	}
+
+	size_t size()
+	{
+		return _size;
+	}
+
+	double mean()
+	{
+		double sum = 0;
+		for (size_t i = 0; i < _size; i++)
+		{
+			sum += extended_array[i];
+		}
+		return sum / _size;
+	}
+
+	double mean(int begin, int end)
+	{
+		if (begin > end || begin < 0 || end > (extended_array.size() - 1)) {
+			throw std::invalid_argument("invalid argument");
+		}
+		double sum = 0;
+		for (size_t i = begin; i <= end; i++)
+		{
+			sum += extended_array[i];
+		}
+		return sum / (end - begin + 1);
+	}
+
+	double median()
+	{
+		std::deque<T> temp_array;
+		std::copy(extended_array.begin(), extended_array.end(), back_inserter(temp_array));
+		std::sort(temp_array.begin(), temp_array.end());
+		if (_size % 2 == 1)
+		{
+			return temp_array[_size / 2];
+		}
+		else
+		{
+			return static_cast<double>(temp_array[(_size / 2) - 1] + temp_array[_size / 2]) / 2;
+		}
+	}
+
+	std::pair<T, int> mode()
+	{
+		T max = extended_array[0], cmax = 0, rmax = 0;
+		for (int i = 0; i < _size; i++) 
+		{
+			if (cmax > rmax) 
+			{
+				rmax = cmax;
+				max = extended_array[i - 1];
+			}
+			cmax = 0;
+			for (int j = i; j < _size; j++)
+			{
+				if (extended_array[j] == extended_array[i])
+				{
+					cmax++;
+				}
+			}
+		}
+		return std::pair<T, int>(max, rmax);
+	}
+
+	int checkSum() 
+	{
+		if (typeid(extended_array[0]) != typeid(int) && typeid(extended_array[0]) != typeid(bool)) {
+			throw std::bad_typeid();
+		}
+		int count{ 0 };
+		for (auto& el : extended_array) {
+			if (static_cast<bool>(el) == true) {
+				count++;
+			}
+		}
+		return count;
+	}
+};
