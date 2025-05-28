e05d838bda7cf7fb2b13e37fba2328a2ccb41c2f
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-05-29 00:13:24 +0300

    Add files via upload

---
+++ cppl-homeworks/homework4_1/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+
+#include "List.h"
+
+TEST_CASE("Testing List base functions", "[List]") {
+	List test_list;
+	CHECK(test_list.Empty() == true);
+	test_list.PushFront(666);
+	test_list.PushFront(42);
+	test_list.PushBack(-1);
+	test_list.PushBack(-28282);
+	CHECK(test_list.Size() == 4);
+	test_list.Clear();
+	CHECK(test_list.Size() == 0);
+	CHECK(test_list.Empty() == true);
+}
+
+int main() {
+	return Catch::Session().run();
+}
