4ac1c3948b92d955ec33d70c1e8b1106ffe52b81
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-06-04 01:10:56 +0300

    Add files via upload

---
+++ cppl-homeworks/homework5_2/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "table_templ.h"
+
+int main() {
+#ifdef _WIN32
+	setlocale(LC_ALL, "rus");
+#endif
+
+	auto test = Table<int>(2, 3);
+	test[0][0] = 4;
+	std::cout << test[0][0]; // выводит 4
+
+	std::cout << "\nРазмер таблицы: " << test.Size();
+
+	return EXIT_SUCCESS;
+}
---
+++ cppl-homeworks/homework5_2/table_templ.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+template<typename T>
+class Table {
+
+private:
+	T** elements{ nullptr };
+	int rows{};
+	int cols{};
+
+public:
+	Table(int rows, int cols) {
+		this->rows = rows;
+		this->cols = cols;
+		elements = new T*[rows];
+		for (int i{ 0 }; i < rows; i++)
+			elements[i] = new T[cols];
+	}
+	~Table() {
+		for (int i{ 0 }; i < rows; i++)
+			delete[] elements[i];
+		delete[] elements;
+		elements = nullptr;
+	}
+
+	const T* operator[](const int row) const {
+		if ( row < 0 || row >= rows ) 
+			throw std::runtime_error("Некорректный индекс");
+		return elements[row];
+	}
+	T* operator[](const int row) {
+		if ( row < 0 || row >= rows ) 
+			throw std::runtime_error("Некорректный индекс");
+		return elements[row];
+	}
+
+	int Size() const {
+		return rows * cols;
+	}
+};
