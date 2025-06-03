b0c487ed3f18804627c5506d63d0ddfc95c97703
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-06-04 01:10:16 +0300

    Add files via upload

---
+++ cppl-homeworks/homework5_1/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+void square(T &a) {
+	a *= a;
+}
+
+template <>
+void square(std::vector<int>& vect) {
+	for (auto& i : vect)
+		i *= i;
+}
+
+int main() {
+	int num{ 4 };
+	std::cout << "[IN]: " << num << std::endl;
+	square(num);
+	std::cout << "[OUT]: " << num << std::endl;
+
+	std::vector<int> vect{ {-1, 4, 8} };
+	std::cout << "[IN]: ";
+	for (auto i{ vect.begin() }; i != vect.end(); i++) {
+		std::cout << *i;
+		if (std::next(i) != vect.end()) {
+			std::cout << ", ";
+		}
+	}
+	square(vect);
+	std::cout << "\n[OUT]: ";
+	for (auto i{ vect.begin() }; i != vect.end(); i++) {
+		std::cout << *i;
+		if (std::next(i) != vect.end()) {
+			std::cout << ", ";
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
