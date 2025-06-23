b1d70f0a7c2188a6d3142dd59c5d975dbbfb49eb
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-06-24 01:07:10 +0300

    Add files via upload

--- cppl-homeworks/homework8_2/main.cpp
+++ cppl-homeworks/homework8_2/main.cpp
@@ -9,9 +9,9 @@ int main() {
 	std::list<std::string> test_list = { "one", "two", "three", "four" };
 	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
 
-	print_container(test_set.begin(), test_set.end());
-	print_container(test_list.begin(), test_list.end());
-	print_container(test_vector.begin(), test_vector.end());
+	print_container(test_set.cbegin(), test_set.cend());
+	print_container(test_list.cbegin(), test_list.cend());
+	print_container(test_vector.cbegin(), test_vector.cend());
 
 	return EXIT_SUCCESS;
 }
