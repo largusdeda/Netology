1ac662d30eb7893069ff0489875d6833b428bbe9
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-07-02 00:54:58 +0300

    Add files via upload

---
+++ cppl-homeworks/homework10_3/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <utility>
+#include "my_uniqueptr.h"
+
+int main() {
+	int a{ 11 };
+	int* ptr_a{ &a };
+	my_uniqueptr<int> uptr_a(ptr_a);
+
+	std::cout << *uptr_a << std::endl;
+
+	int b{ 10 };
+	int* ptr_b{ &b };
+	my_uniqueptr<int> uptr_b(ptr_b);
+
+	//uptr_b = uptr_a;
+	uptr_b = std::move(uptr_a);
+	//std::cout << *uptr_a << std::endl;
+	std::cout << *uptr_b << std::endl;
+
+	my_uniqueptr<int> uptr_c(std::move(uptr_b));
+	std::cout << *uptr_c << std::endl;
+	*uptr_c = 22;
+
+	ptr_a = uptr_c.release();
+	std::cout << *ptr_a << std::endl;
+
+	return EXIT_SUCCESS;
+}
---
+++ cppl-homeworks/homework10_3/my_uniqueptr.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+template <typename T>
+class my_uniqueptr {
+private:
+	T* u_ptr{ nullptr };
+public:
+	my_uniqueptr<T>(T* ptr) : u_ptr{ ptr } {};
+
+	my_uniqueptr<T>(const my_uniqueptr& other) = delete;
+
+	my_uniqueptr<T>(my_uniqueptr&& other) noexcept {
+		u_ptr = other.u_ptr;
+		other.u_ptr = nullptr;
+	}
+
+	~my_uniqueptr<T>() {
+		u_ptr = nullptr;
+	}
+
+	my_uniqueptr<T>& operator=(const my_uniqueptr& other) = delete;
+
+	my_uniqueptr<T>& operator=(my_uniqueptr&& other) noexcept {
+		if (this != &other) {
+			u_ptr = other.u_ptr;
+			other.u_ptr = nullptr;
+		}
+		return *this;
+	}
+
+	T& operator*() const {
+		return *u_ptr;
+	}
+
+	T* release() {
+		T* rawptr = u_ptr;
+		u_ptr = nullptr;
+		return rawptr;
+	}
+};
