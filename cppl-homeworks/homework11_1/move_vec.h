09cb14ecbce55f7c5c8e2c510edeaf4d5d44a873
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-07-11 03:31:19 +0300

    Add files via upload

--- cppl-homeworks/homework11_1/move_vec.h
+++ cppl-homeworks/homework11_1/move_vec.h
@@ -1,8 +1,7 @@
 ﻿#pragma once
 
 template <typename T>
-void move_vectors(std::vector<T>& source, std::vector<T>& result) {
-	if (&source == &result)
-		return;
-	result = std::move(source);
+void move_vectors(std::vector<T>& source, std::vector<T>& dest) {
+	if (&source != &dest)
+		dest = std::move(source);
 }
