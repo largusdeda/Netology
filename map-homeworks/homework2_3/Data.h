4df611c174e8d10b2d6d5833e48615d8b3e5430f
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-09-05 02:10:12 +0300

    Add files via upload

---
+++ map-homeworks/homework2_3/Data.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <mutex>
+
+class Data {
+
+public:
+	Data(int x, int y, int z);
+
+	void print();
+
+	std::mutex& get_mutex();
+
+	// std::lock
+	static void lock_swap(Data& A, Data& B);
+	// std::scoped_lock
+	static void scoped_lock_swap(Data& A, Data& B);
+	// std::unique_lock
+	static void unique_lock_swap(Data& A, Data& B);
+
+private:
+	int x, y, z;
+	std::mutex m;
+
+};
