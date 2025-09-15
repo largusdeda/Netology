99b28a3bff2a610b96eb52329003ae0a0602a8ab
Ларгус деда <130408211+largusdeda@users.noreply.github.com>
2025-09-15 23:52:04 +0300

    Add files via upload

---
+++ map-homeworks/docker/2/async_for_each.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <thread>
+#include <future>
+#include <iterator>
+#include <algorithm>
+
+template<typename It, typename Func>
+void async_for_each(It begin, It end, Func func) {
+	static unsigned int threads{ std::thread::hardware_concurrency() / 2};
+	long long curr_size{ std::distance(begin, end) };
+	if (curr_size <= 0)
+		return;
+
+	static long long block_size{ curr_size / threads };
+	if (curr_size <= block_size || block_size == 0) {
+		std::for_each(begin, end, func);
+	}
+	else {
+		It mid{ begin + curr_size / 2 };
+		std::future<void> first_half{ std::async(std::launch::async, &async_for_each<It, Func>, begin, mid, func) };
+		async_for_each(mid, end, func);
+		first_half.get();
+	}
+}
---
+++ map-homeworks/docker/3/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include "async_for_each.h"
+
+int main() {
+#ifdef _WIN32
+	setlocale(LC_ALL, "rus");
+#endif
+
+	std::vector<double> v(1'000'000, 0.5);
+
+	auto start{ std::chrono::steady_clock::now() };
+	async_for_each(v.begin(), v.end(), [](double& value) { value *= 2; });
+	auto finish{ std::chrono::steady_clock::now() };
+
+	std::chrono::duration<double, std::milli> delta{ std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start) };
+	std::cout << "Время работы функции: " << delta.count() << " ms";
+
+	return EXIT_SUCCESS;
+}
